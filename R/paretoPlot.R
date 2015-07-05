d9d0ca3ea0e0b78126f68df6e4e04770ac3b327f
Kevin Dunn <kgdunn@gmail.com>
2015-07-05 14:14:54 -0400

    Updated to v0.12 of the package

--- R/paretoPlot.R
+++ R/paretoPlot.R
@@ -1,7 +1,7 @@
 paretoPlot <- function(lsmodel){
   # This code draws a Pareto plot; it requires the "ggplot2" library
   # install.packages("ggplot2", dependencies = TRUE)
-  require(ggplot2)
+  # require(ggplot2)
   
   # Extract all the coefficients, except for the intercept
   coeff.full <- coef(lsmodel)[2:length(coef(lsmodel))]
@@ -23,6 +23,11 @@ paretoPlot <- function(lsmodel){
     value=coeff$x,
     group=grouping
   )
+  
+  # Make this work to get the scipt uploaded into CRAN
+  # https://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
+  label <- value <- group <- NULL # Setting the variables to NULL first
+  
   p <- ggplot(dat, aes(x=label, y=value, fill=group)) + 
     geom_bar(stat="identity") +
     coord_flip() + theme_bw() +
